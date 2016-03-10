acc50296ddb49f872d2025864ae3b9ffaa3b8477
Nosinden <harris.twoure@yandex.com>
2016-03-10 00:40:53 -0600

    updated for compatibility with site updates
    Not finished, need to fix list and add search back. Will setup DumbTools too.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,143 +1,131 @@
 ####################################################################################################
 #                                                                                                  #
-#                               hanime.tv Plex Channel -- v0.01                                    #
+#                                     hanime.tv Plex Channel                                       #
 #                                                                                                  #
 ####################################################################################################
 # import modules
-import urllib, json
+import messages
 
-# set global variables
+# set global variablesi
 PREFIX = '/video/hanimetv'
 TITLE = 'Hanime.tv'
-BASE_URL = 'https://hanime.tv/'
+BASE_URL = 'https://hanime.tv'
+SEARCH_BASE_URL = 'https://one-piece.hanime.tv'
 ICON = 'icon-default.png'
 ART = 'art-default.jpg'
 
-####################################################################################################
+USER_AGENT = (
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    'Chrome/46.0.2490.86 Safari/537.36'
+    )
 
+MC = messages.NewMessageContainer(PREFIX, TITLE)
+
+####################################################################################################
 def Start():
     ObjectContainer.art = R(ART)
     ObjectContainer.title1 = TITLE
 
     DirectoryObject.thumb = R(ICON)
+    DirectoryObject.art = R(ART)
+
+    InputDirectoryObject.art = R(ART)
+    VideoClipObject.art = R(ART)
 
     HTTP.CacheTime = 0
-    HTTP.Headers['User-Agent'] = (
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-        'Chrome/46.0.2490.86 Safari/537.36'
-        )
-    HTTP.Headers['Referer'] = BASE_URL + '/browse'
-    HTTP.Headers['Origin'] = BASE_URL
-
-    Dict['Api Env'] = LoadEnvSettings()
-    Log(Dict['Api Env'])
-    Dict.Save()
-
-    HTTP.Headers['host'] = Dict['Api Env']['app_id'] + '-dsn.algolia.net'
 
 ####################################################################################################
-# Create the main menu
-
-@handler(PREFIX, TITLE, ICON, ART)
+@handler(PREFIX, TITLE, thumb=ICON, art=ART)
 def MainMenu():
+    """Create the Main Menu"""
+
     oc = ObjectContainer(title2=TITLE)
 
+    #q=&search_by=all&sort_by=name.raw&sort_by_ordering=desc&search_from=0&page_size=30
     oc.add(DirectoryObject(
-        key=Callback(DirectoryList, url=BASE_URL, page=0, order='', title='Latest'),
-        title='Latest'))
-    oc.add(DirectoryObject(
-        key=Callback(DirectoryList, url=BASE_URL, page=0, order='_views_desc', title='Views'),
-        title='Views'))
-    oc.add(DirectoryObject(
-        key=Callback(DirectoryList, url=BASE_URL, page=0, order='_favorites_count_desc', title='Favorites'),
-        title='Favorites'))
-    oc.add(DirectoryObject(
-        key=Callback(DirectoryList, url=BASE_URL, page=0, order='_released_at_desc', title='Realse Date'),
-        title='Release Date'))
-    oc.add(DirectoryObject(
-        key=Callback(DirectoryList, url=BASE_URL, page=0, order='_alphabetical_desc', title='ABC'),
+        key=Callback(DirectoryList, page=0, sort_by='name.raw', title='ABC'),
         title='Alphabetical'))
+    #q=&search_by=all&sort_by=created_at&sort_by_ordering=desc&search_from=0&page_size=48
+    oc.add(DirectoryObject(
+        key=Callback(DirectoryList, page=0, sort_by='created_at', title='Upload Date'),
+        title='Upload Date'))
+    #q=&search_by=all&sort_by=released_at&sort_by_ordering=desc&search_from=0&page_size=48
+    oc.add(DirectoryObject(
+        key=Callback(DirectoryList, page=0, sort_by='released_at', title='Release Date'),
+        title='Release Date'))
+    #q=&search_by=all&sort_by=views&sort_by_ordering=desc&search_from=0&page_size=48
+    oc.add(DirectoryObject(
+        key=Callback(DirectoryList, page=0, sort_by='views', title='Views'),
+        title='Views'))
+    #q=&search_by=all&sort_by=favorites_count&sort_by_ordering=desc&search_from=0&page_size=48
+    oc.add(DirectoryObject(
+        key=Callback(DirectoryList, page=0, sort_by='favorites_count', title='Favorites'),
+        title='Favorites'))
+    """
     oc.add(InputDirectoryObject(
         key=Callback(Search),
         title='Search', summary='Search Hanime.tv', prompt='Search for...'))
-
+    """
     return oc
 
 ####################################################################################################
-# directory does the heavy lifting
-
 @route(PREFIX + '/directorylist', page=int)
-def DirectoryList(url, page, order, title, query=''):
-    title2=title
+def DirectoryList(page, sort_by, title, query=''):
+    """Directory does the Heavy lifting"""
 
-    if not query:
-        query = ''
+
+    page_size = 48  # currently this cannot change, the site dosen't allow it
+    search_from = page_size * page if page > 0 else page
+    query = String.Quote(query, usePlus=True) if query else ''
+
+    req_data = (
+        'q=%s&search_by=all&sort_by=%s&sort_by_ordering=desc&search_from=%i&page_size=%i'
+        %(query, sort_by, search_from, page_size)
+        )
+
+    h = {
+        'origin': BASE_URL, 'referer': BASE_URL + '/search', 'user-agent': USER_AGENT,
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+
+    req = HTTP.Request(SEARCH_BASE_URL, data=req_data, headers=h)
+    data = JSON.ObjectFromString(req.content)
+
+    if data['hits']['total'] == 0:
+        return MC.message_container('Warning', 'Page Empty')
+
+    total = int(data['hits']['total'])
+    p, r = divmod(total, page_size)
+    last_page = p + 1 if r > 0 else p
+    next_page = page + 1
+
+    if total <= page_size:
+        main_title = title
+    elif (total > page_size) and (page != last_page):
+        main_title = '%s / Page %i of %i' %(title, page, last_page)
     else:
-        query = String.Quote(query, usePlus=False)
-
-    hitsPerPage = 20
-
-    form_data = {"params":
-            urllib.urlencode({
-                'query': query, 'hitsPerPage': hitsPerPage, 'page': page,
-                'facets': 'brand', 'facetFilters': '[[]]'
-                })
-            }
-
-    Log(form_data)
-
-    algolia_api_data = urllib.urlencode({
-        'x-algolia-api-key': Dict['Api Env']['api_key'],
-        'x-algolia-application-id': Dict['Api Env']['app_id'],
-        'x-algolia-agent': Dict['Api Env']['algolia_agent']
-        })
-
-    host_api_url = 'https://' + Dict['Api Env']['app_id'] + '-dsn.algolia.net'
-
-    if order:
-        url = host_api_url + '/1/indexes/HentaiVideo%s/query?' %order + algolia_api_data
-    else:
-        url = host_api_url + '/1/indexes/HentaiVideo/query?' + algolia_api_data
-
-    r = HTTP.Request(url, data=json.dumps(form_data))
-    data = JSON.ObjectFromString(r.content)
-
-    if not data['nbHits'] > 0:
-        return MessageContainer('Warning', 'Page Empty')
-    else:
-        pass
-
-    if int(data['nbPages']) > 0 and int(data['nbPages']) > int(page):
-        main_title = '%s | Page %i of %i' %(title2, int(page), int(data['nbPages']))
-    else:
-        main_title = '%s | Page %i | Last Page' %(title2, int(page))
+        main_title = '%s / Page %i / Last Page' %(title, page)
 
     oc = ObjectContainer(title2=main_title)
 
-    for node in data['hits']:
-        name = node['name']
-        subs = node['hard_subtitle_language']
-        if subs == 'English':
-            title = name + ' (Sub)'
-        else:
-            title = name
-
-        slug = node['slug']
-        views = node['views']
-        fav = node['favorites_count']
-        tagline = 'Favorites: %s | Views: %s' %(views, fav)
-        #created_at = node['created_at']
-        released_at = node['released_at']
-        description = String.StripTags(node['description'])
-        tags = node['_tags']
-        studio = node['brand']
-        cover_url = node['cover_url']
-        video_url = Dict['Api Env']['url'] + '/api/v1/hentai_videos/' + slug + '/interest'
-        Log('video interest url = %s' %video_url)
+    for d in data['hits']['hits']:
+        name = d['_source']['name']
+        subs = d['_source']['is_hard_subtitled']
+        ntitle = name + ' (Sub)' if subs else name
+        slug = d['_source']['slug']
+        views = d['_source']['views']
+        fav = d['_source']['favorites_count']
+        tagline = 'Favorites: %s | Views: %s' %(fav, views)
+        released_at = d['_source']['released_at']
+        description = String.StripTags(d['_source']['description'])
+        tags = [x['text'] for x in d['_source']['hentai_tags']]
+        brand = d['_source']['brand']
+        cover_url = d['_source']['cover_url']
+        video_url = BASE_URL + '/hentai-videos/' + slug
 
         oc.add(
             VideoClipObject(
-                title=title,
+                title=ntitle,
                 summary=description,
                 thumb=cover_url,
                 url=video_url,
@@ -145,28 +133,26 @@ def DirectoryList(url, page, order, title, query=''):
                 source_title='hanime.tv',
                 originally_available_at=Datetime.ParseDate(released_at),
                 year=int(Datetime.ParseDate(released_at).year),
-                studio=studio,
-                directors=[studio],
+                studio=brand,
+                directors=[brand],
                 tagline=tagline,
                 content_rating='X'
                 )
             )
 
-    if int(data['nbPages']) > 0 and int(data['nbPages']) > int(page):
+    if next_page < last_page:
         oc.add(NextPageObject(
             key=Callback(
-                DirectoryList, url=url, page=int(page) + 1, order=order,
-                title=title2, query=query),
+                DirectoryList, page=next_page, sort_by=sort_by,
+                title=title, query=query),
             title='Next Page>>'))
 
     if len(oc) > 0:
         return oc
     else:
-        return MessageContainer(header='Warning', message='Page Empty')
-
+        return MC.message_container('Warning', 'Page Empty')
+"""
 ####################################################################################################
-# Search
-
 @route(PREFIX + '/search')
 def Search(query=''):
     Log.Info('search term = %s' %query)
@@ -176,13 +162,13 @@ def Search(query=''):
     page = 0
 
     form_data = {'params':
-        urllib.urlencode({
+        urllib2.urlencode({
             'query': query, 'hitsPerPage': hitsPerPage, 'page': page,
             'facets': 'brand', 'facetFilters': '[[]]'
             })
         }
 
-    algolia_api_data = urllib.urlencode({
+    algolia_api_data = urllib2.urlencode({
         'x-algolia-api-key': Dict['Api Env']['api_key'],
         'x-algolia-application-id': Dict['Api Env']['app_id'],
         'x-algolia-agent': Dict['Api Env']['algolia_agent']
@@ -231,10 +217,10 @@ def Search(query=''):
     return oc
 
 ####################################################################################################
-# load defaults
-
 @route(PREFIX + '/loadenvsettings')
 def LoadEnvSettings():
+    \"\"\"Load Defaults\"\"\"
+
     # find algolia api settings from env_settings.js
     page_text = HTTP.Request(BASE_URL).content
     env_settings_url = 'https:' + Regex('src=\"(.+env_settings.+?)\"').search(page_text).group(1)
@@ -259,4 +245,4 @@ def LoadEnvSettings():
         }
 
     return api_settings
-
+"""
